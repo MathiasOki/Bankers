4f3dce354c2893914049f11632036e98ec3743e0
Mathias Tollerud <mathias.tollerud@gmail.com>
2017-03-02 10:30:23 +0100

    Small change

--- index.php
+++ index.php
@@ -69,7 +69,6 @@
                         Mål
                     </div>
                     <div class="panel-body">
-
 						<h1>Velkommen, Mathias!</h1>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -99,7 +98,7 @@
                         </form>
                     </div>
                 </div>
-				
+
 				<div class="panel panel-default">
 					<div class="panel-heading">
 						<ul class="nav nav-tabs">
