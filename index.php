e046d8f0ab1c8e613e0ab47f001aaf50bbf9aa00
Mathias Tollerud <mathias.tollerud@gmail.com>
2017-04-19 11:23:43 +0200

    Fixed payment and transfer at dasgboard

--- index.php
+++ index.php
@@ -11,6 +11,150 @@
 
 	require_once("assets/common/inc/head.php");
 	require_once("assets/common/inc/navbar.php");
+
+	$error = false;
+
+	function countDigits($str)
+	{
+	    return preg_match_all( "/[0-9]/", $str);
+	}
+
+	if( isset($_POST['payment']) ) {
+
+		// prevent injections/ clear user invalid inputs
+		$from = trim($_POST['accountFrom']);
+		$from = strip_tags($from);
+		$from = htmlspecialchars($from);
+
+		$to = trim($_POST['accountTo']);
+		$to = strip_tags($to);
+		$to = htmlspecialchars($to);
+
+		$msg = trim($_POST['message']);
+		$msg = rawurlencode($msg);
+
+		$kroner = trim($_POST['kroner']);
+		$kroner = strip_tags($kroner);
+		$kroner = htmlspecialchars($kroner);
+
+		$ore = trim($_POST['ore']);
+		$ore = strip_tags($ore);
+		$ore = htmlspecialchars($ore);
+
+		if(empty($_POST['recurring'])){
+			$recurring = 0;
+		} else {
+			$recurring = $_POST['recurring'];
+		}
+
+		if(empty($from)){
+			$error = true;
+			$errorMessage = "Du må skrive inn fra-konto.";
+		}
+
+		if(empty($to)){
+			$error = true;
+			$errorMessage = "Du må skrive inn til-konto.";
+		}
+
+		if(empty($msg)){
+			$msg = "%20";
+		}
+
+		if(countDigits($to) < 11){
+			$error = true;
+			$errorMessage = "Kononummeret eksisterer ikke.";
+		}
+
+		if(empty($kroner)){
+
+			$kroner = 0;
+
+			//$error = true;
+			//$errorMessage = "Du må skrive inn kroner.";
+		}
+
+		if(empty($ore)){
+			$error = true;
+			$errorMessage = "Du må skrive inn øre.";
+		}
+
+		if(!$error){
+			$interval = 0;
+			$endDate = 0;
+
+			$data = $satan->payment($msg, $from, $to, $kroner, $ore, $recurring, $interval, $endDate);
+
+			if ($data == true) {
+				$successMessage = "Pengene er overført.";
+			} else {
+				$error = true;
+				$errorMessage = "En feil skjedde. Prøv igjen.";
+			}
+		}
+	}
+
+	if( isset($_POST['transfer']) ) {
+
+		// prevent injections/ clear user invalid inputs
+		$from = trim($_POST['accountFrom']);
+		$from = strip_tags($from);
+		$from = htmlspecialchars($from);
+
+		$to = trim($_POST['accountTo']);
+		$to = strip_tags($to);
+		$to = htmlspecialchars($to);
+
+		$msg = trim($_POST['message']);
+		$msg = rawurlencode($msg);
+
+		$kroner = trim($_POST['kroner']);
+		$kroner = strip_tags($kroner);
+		$kroner = htmlspecialchars($kroner);
+
+		$ore = trim($_POST['ore']);
+		$ore = strip_tags($ore);
+		$ore = htmlspecialchars($ore);
+
+		if(empty($from)){
+			$error = true;
+			$errorMessage = "Du må skrive inn fra-konto.";
+		}
+
+		if(empty($to)){
+			$error = true;
+			$errorMessage = "Du må skrive inn til-konto.";
+		}
+
+		if(empty($msg)){
+			$msg = "%20";
+		}
+
+		if(empty($kroner)){
+
+			$kroner = 0;
+
+			//$error = true;
+			//$errorMessage = "Du må skrive inn kroner.";
+		}
+
+		if(empty($ore)){
+			$error = true;
+			$errorMessage = "Du må skrive inn øre.";
+		}
+
+		if(!$error){
+			$data = $satan->internalTransfer($msg, $from, $to, $kroner, $ore);
+
+			if ($data == true) {
+				$successMessage = "Pengene er overført.";
+			}
+			if ($data == false) {
+				$error = true;
+				$errorMessage = "En feil skjedde. Prøv igjen.";
+			}
+		}
+	}
 ?>
 <div class="flex-container">
 
@@ -99,142 +243,180 @@
 							<li><a href="#vipps" data-toggle="tab">VIPPS</a></li>
 						</ul>
 					</div>
-					<form rel="form">
-						<div class="tab-content">
-							<div class="tab-pane panel-form active" id="betale">
-								<div class="panel-body">
-										<div class="row">
-											<div class="form-group col-md-6">
-											  <label for="tlf">Fra konto</label>
-											  <input type="number" class="form-control" id="tlf" placeholder="Skriv eller søk..." required="">
-											</div>
+					<div class="tab-content">
+						<div class="tab-pane panel-form active" id="betale">
+							<div class="panel-body">
+								<div class="row">
+									<div class="col-md-12">
+										<form class="form-horizontal form-custom" method="post" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>">
+											<div class="row">
+												<div class="col-sm-6 border-right">
+													<div class="form-group">
+														<label for="accountFrom">Fra konto</label>
+														<select class="form-control" name="accountFrom" required="">
+															<option selected disabled>Velg konto</option>
+															<?php
+																$result = $satan->getAccounts($logged['customerID']);
+																foreach($result as $row) {
+															?>
+															<option value="<?=$row['accountNumber']?>"><?=$row['accountType']?> (<?=$customClass->makeAccountNumber($row['accountNumber'])?>)</option>
+															<?php
+																}
+															?>
+														</select>
+													</div>
+												</div>
 
-											<div class="form-group col-md-6">
-											  <label for="sum">Til konto</label>
-											  <input type="number" class="form-control" id="sum" placeholder="Skriv eller søk...." required="">
-											</div>
-										</div>
-
-
-										<div class="row">
-											<div class="form-group col-md-8">
-											  <label for="sum">KID/melding</label>
-											  <input type="number" class="form-control" id="sum" placeholder="skriv her..." required="">
-											</div>
-
-											<div class="form-group col-md-4">
-											  <label for="sum">Beløp</label>
-											  <input type="number" class="form-control" placeholder="00.00" required="">
-											</div>
-										</div>
-
-										<div class="row">
-											<div class="form-group col-md-6">
-											  <label for="sum">Motakers navn</label>
-											  <input type="number" class="form-control" placeholder="Navn navnesen..." required="">
-											</div>
-
-											<div class="form-group col-md-4">
-											  <label for="sum">dato</label>
-											  <input type="number" class="form-control" placeholder="dd/mm/yyyy..." required="">
-											</div>
-										<div class="form-group col-md-2">
-											<p><b>Fast?</b></p>
-											<div class="dropdown">
-												  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-													Nei
-													<span class="caret"></span>
-												  </button>
-												  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-														<li><a href="#">Daglig</a></li>
-														<li><a href="#">Ukentlig</a></li>
-														<li><a href="#">Annenhver uke</a></li>
-														<li role="separator" class="divider"></li>
-														<li><a href="#">Månedlig</a></li>
-														<li><a href="#">Kvartal</a></li>
-														<li><a href="#">Årlig</a></li>
-												  </ul>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div class="tab-pane panel-form" id="overfore">
-								<div class="panel-body">
-										<div class="row">
-											<div class="form-group col-md-5">
-											  <label for="tlf">Fra konto</label>
-											  <input type="number" class="form-control" id="tlf" placeholder="Skriv eller søk..." required="">
-											</div>
-
-											<div class="form-group col-md-5">
-											  <label for="sum">Til konto</label>
-											  <input type="number" class="form-control" id="sum" placeholder="Skriv eller søk...." required="">
-											</div>
-											<div class="form-group col-md-2">
-												<p><b>Fast?</b></p>
-												<div class="dropdown">
-													  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-														Nei
-														<span class="caret"></span>
-													  </button>
-													  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-															<li><a href="#">Daglig</a></li>
-															<li><a href="#">Ukentlig</a></li>
-															<li><a href="#">Annenhver uke</a></li>
-															<li role="separator" class="divider"></li>
-															<li><a href="#">Månedlig</a></li>
-															<li><a href="#">Kvartal</a></li>
-															<li><a href="#">Årlig</a></li>
-													  </ul>
+												<div class="col-sm-6">
+													<div class="form-group">
+														<label for="accountTo">Til konto</label>
+														<input type="number" class="form-control" name="accountTo" required="">
+													</div>
 												</div>
 											</div>
-										</div>
 
+											<hr>
 
-										<div class="row">
-											<div class="form-group col-md-6">
-											  <label for="sum">Melding</label>
-											  <input type="number" class="form-control" id="sum" placeholder="skriv her..." required="">
+											<div class="form-group">
+												<label for="message">Melding</label>
+												<textarea class="form-control" rows="5" name="message"></textarea>
 											</div>
 
-											<div class="form-group col-md-3">
-											  <label for="sum">Beløp</label>
-											  <input type="number" class="form-control" placeholder="00.00" required="">
-											</div>
-											<div class="form-group col-md-3">
-											  <label for="sum">dato</label>
-											  <input type="number" class="form-control" placeholder="dd/mm/yyyy..." required="">
-											</div>
-										</div>
-								</div>
-							</div>
+											<hr>
 
-							<div class="tab-pane panel-form" id="vipps">
-								<div class="panel-body">
-										<div class="row">
-											<div class="form-group col-md-4">
-											  <label for="tlf">Til</label>
-											  <input type="number" class="form-control" id="tlf" placeholder="Tlfnummer eller søk etter person" required="">
+											<div class="row valign">
+												<div class="col-sm-6 border-right">
+													<div class="form-group">
+														<label for="">Overføringsdato</label>
+														<p>(Her kommer en datepicker)</p>
+													</div>
+
+													<div class="form-group">
+														<label for="">Er dette en fast overføring?</label><br>
+														<input type="checkbox" data-toggle="toggle" data-on="Ja" data-off="Nei" name="recurring" value="1">
+													</div>
+												</div>
+
+												<div class="col-sm-6">
+													<div class="form-group">
+														<label for="kroner">Beløp (kroner og øre)</label>
+														<div class="row">
+															<div class="col-xs-8">
+																<input class="form-control" type="number" name="kroner" required="">
+															</div>
+															<div class="col-xs-4">
+																<input class="form-control" type="number" name="ore" value="00" required="">
+															</div>
+														</div>
+													</div>
+												</div>
 											</div>
-											<div class="form-group col-md-4">
-											  <label for="sum">Melding</label>
-											  <input type="number" class="form-control" id="sum" placeholder="skriv melding her..." required="">
+
+											<hr class="margin-none margin-bottom margin-top">
+
+											<div class="row padding-bottom">
+												<div class="col-md-12">
+													<button type="submit" name="payment" class="btn btn-bankers btn-block">Betal</button>
+												</div>
 											</div>
-											<div class="form-group col-md-4">
-											  <label for="sum">Beløp</label>
-											  <input type="number" class="form-control" placeholder="00.00" required="">
-											</div>
+										</form>
 									</div>
 								</div>
 							</div>
 						</div>
 
-						<div class="panel-footer text-right">
-									<button type="submit" class="btn btn-primary">Betale</button>
+						<div class="tab-pane panel-form" id="overfore">
+							<div class="panel-body">
+								<div class="row">
+									<div class="col-md-12">
+										<form class="form-horizontal form-custom" method="post" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>">
+											<div class="row">
+												<div class="col-sm-6 border-right">
+													<div class="form-group">
+														<label for="accountFrom">Fra konto</label>
+														<select class="form-control" name="accountFrom" required="">
+															<option selected disabled>Velg konto</option>
+															<?php
+																$result = $satan->getAccounts($logged['customerID']);
+																foreach($result as $row) {
+															?>
+															<option value="<?=$row['accountNumber']?>"><?=$row['accountType']?> (<?=$customClass->makeAccountNumber($row['accountNumber'])?>)</option>
+															<?php
+																}
+															?>
+														</select>
+													</div>
+												</div>
+
+												<div class="col-sm-6">
+													<div class="form-group">
+														<label for="accountTo">Til konto</label>
+														<select class="form-control" name="accountTo" required="">
+															<option selected disabled>Velg konto</option>
+															<?php
+																$result = $satan->getAccounts($logged['customerID']);
+																foreach($result as $row) {
+															?>
+															<option value="<?=$row['accountNumber']?>"><?=$row['accountType']?> (<?=$customClass->makeAccountNumber($row['accountNumber'])?>)</option>
+															<?php
+																}
+															?>
+														</select>
+													</div>
+												</div>
+											</div>
+
+											<hr>
+
+											<div class="form-group">
+												<label for="message">Melding</label>
+												<textarea class="form-control" rows="5" name="message"></textarea>
+											</div>
+
+											<hr>
+
+											<div class="row">
+												<div class="col-sm-6 border-right">
+													<div class="form-group">
+														<label for="">Dato</label>
+														<p>(Her kommer en datepicker)</p>
+													</div>
+												</div>
+
+												<div class="col-sm-6">
+													<div class="form-group">
+														<label for="kroner">Beløp (kroner og øre)</label>
+														<div class="row">
+															<div class="col-xs-8">
+																<input class="form-control" type="number" name="kroner" required="">
+															</div>
+															<div class="col-xs-4">
+																<input class="form-control" type="number" name="ore" value="00" required="">
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+
+											<hr class="margin-none margin-bottom margin-top">
+
+											<div class="row padding-bottom">
+												<div class="col-md-12 padding-none">
+													<button type="submit" name="transfer" class="btn btn-bankers btn-block">Overfør</button>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
 						</div>
-					</form>
+
+						<div class="tab-pane panel-form" id="vipps">
+							<div class="panel-body">
+								Vipps kommer snart!
+							</div>
+						</div>
+					</div>
 				</div>
 
 					<div class="panel panel-default">
