5b307815a647bbc334563059e770257f4820a6bc
sauenMalin <sauenmalin@gmail.com>
2017-04-19 10:40:20 +0200

    Changed the icon size and removed :

--- assets/common/inc/footer.php
+++ assets/common/inc/footer.php
@@ -12,7 +12,7 @@
 				<tbody>
 					<tr>
 						<td>
-							<i class="fa fa-envelope fa-lg" aria-hidden="true"></i> Email:
+							<i class="fa fa-envelope fa-lg" aria-hidden="true"></i> Epost
 						</td>
 						<td>
 							kontakt@dnb.no
@@ -20,7 +20,7 @@
 					</tr>
 					<tr>
 						<td>
-							<i class="fa fa-phone fa-lg" aria-hidden="true"></i> Telefon:
+							<i class="fa fa-phone fa-lg" aria-hidden="true"></i> Telefon
 						</td>
 						<td>
 							04800
@@ -28,7 +28,7 @@
 					</tr>
 					<tr>
 						<td>
-							<i class="fa fa-home fa-lg" aria-hidden="true"></i> Hovedkontor:
+							<i class="fa fa-home fa-lg" aria-hidden="true"></i> Hovedkontor
 						</td>
 						<td>
 							Dronning Eufemias gate 30,<br />0191 Oslo
@@ -36,7 +36,7 @@
 					</tr>
 					<tr>
 						<td>
-							<i class="fa fa-inbox fa-lg" aria-hidden="true"></i> Postadresse:
+							<i class="fa fa-inbox fa-lg" aria-hidden="true"></i> Postadresse
 						</td>
 						<td>
 							DNB, Postboks 1600 Sentrum, <br />0021 Oslo
@@ -49,13 +49,13 @@
 			<h3>Sosiale medier</h3>
 			<ul class="list-unstyled">
 				<li>
-					<a href="www.facebook.com"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i> Facebook side</a>
+					<a href="www.facebook.com"><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i> Facebook</a>
 				</li>
 				<li>
-					<a href="www.instagram.com"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i> Instagram konto</a>
+					<a href="www.instagram.com"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i> Instagram</a>
 				</li>
 				<li>
-					<a href="www.twitter.com"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i> Twitter konto</a>
+					<a href="www.twitter.com"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i> Twitter</a>
 				</li>
 			</ul>
 		</div>
