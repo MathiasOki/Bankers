484557f19932e4287ca3c9fb8577765d3db2e80f
sauenMalin <sauenmalin@gmail.com>
2017-04-20 14:08:02 +0200

    Changed to real data

--- regularPayment.php
+++ regularPayment.php
@@ -72,16 +72,16 @@
 									  <input type="number" class="form-control" placeholder="Navn navnesen..." required="">
 									</div>
 
-									<div class="form-group col-md-4">
+									<div class="form-group col-md-3">
 									  <label for="sum">Dato</label>
 									  <input type="number" class="form-control" placeholder="dd/mm/yyyy..." required="">
 									</div>
 
-									<div class="form-group col-md-2">
-										<p><b>Fast?</b></p>
+									<div class="form-group col-md-3">
+										<p><b>Hvor ofte</b></p>
 										<div class="dropdown">
 											  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-											    Nei
+											    Månedlig
 											    <span class="caret"></span>
 											  </button>
 											  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
@@ -139,22 +139,22 @@
 								</thead>
 								<tbody>
 									<tr>
-										<td>Brukskonto</td>
-										<td>Regningkonto</td>
+										<td>Lønnskonto</td>
+										<td>Pengebingen</td>
 										<td>Månedlig</td>
 										<td>4000.00</td>
 										<td><span class="glyphicon glyphicon-pencil"></span><span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-menu-down"></span></td>
 									</tr>
 									<tr>
-										<td>Regningkonto <br /> 1234.56.78910</td>
-										<td>Navn Navnesen <br />1098.76.54321</td>
+										<td>Pengebingen <br /> 1234.56.78910</td>
+										<td>Sana Gulbrandsen <br />1098.76.54321</td>
 										<td>Månedlig <br />Dag: <br />Siste dag i måneden</td>
-										<td>6000.00</td>
+										<td>500.00</td>
 										<td><span class="glyphicon glyphicon-pencil"></span><span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-menu-up"></span></td>
 									</tr>
 									<tr>
-										<td>Brukskonto</td>
-										<td>Sparekonto</td>
+										<td>Lønnskonto</td>
+										<td>Pengebingen</td>
 										<td>Månedlig</td>
 										<td>500.00</td>
 										<td><span class="glyphicon glyphicon-pencil"></span><span class="glyphicon glyphicon-remove"></span><span class="glyphicon glyphicon-menu-down"></span></td>
